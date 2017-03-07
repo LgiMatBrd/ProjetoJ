c61371c66bd1f8b09f1f0d57cf92c7faedfdc227
LgiMatBrd <contato@skorp.com.br>
2017-03-06 22:40:58 -0300

    Listagem de itens vistoriados
    Listagem de vistorias adicionada, próxima etapa é adicionar página de
    vistorias individuais.

--- AppVistoriasXDK/www/app/app.js
+++ AppVistoriasXDK/www/app/app.js
@@ -7,61 +7,39 @@ app.config(function($routeProvider) {
         templateUrl : "paginas/clientes.html",
 		controller  : 'homeController'
     })
-    .when("/vistorias/:showName?", {
+    .when("/vistorias/:id?", {
         templateUrl : "paginas/vistorias.html",
 		controller  : 'vistoriasController'
     });
 });
 
 app.controller('vistoriasController', function($scope, $routeParams, $http) {
-	$scope.showName=$routeParams.showName
+	$scope.id= $routeParams.id;
     $scope.goBack = function() {
-  window.history.back();
-};
- /* getItem(); // Load all available items 
-  function getItem(){  
-  $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/getItem.php").success(function(data){
+        window.history.back();
+    };
+     
+    getItem(); // Load all available items 
+    function getItem(){  
+    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/vistorias/getVistorias.php?id_cliente="+$scope.id).success(function(data){
         $scope.items = data;
        });
-  };
-  
-  $scope.addItem = function (item) {
-    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/addItem.php?item="+item).success(function(data){
+    };
+
+    $scope.addItem = function (item) {
+    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/vistorias/addVistorias.php?item="+item).success(function(data){
         getItem();
         $scope.itemInput = "";
       });
-  };
-  
-  $scope.deleteItem = function (item) {
-    if(confirm("Are you sure to delete this item?")){
-    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/deleteItem.php?itemID="+item).success(function(data){
-        getItem();
-      });
-    }
-  };
-
-  $scope.clearItem = function () {
-    if(confirm("Delete all checked items?")){
-    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/clearItem.php").success(function(data){
-        getItem();
-      });
-    }
-  };  
-
-  $scope.changeStatus = function(item, status, task) {
-    if(status=='2'){status='0';}else{status='2';}
-      $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/updateItem.php?itemID="+item+"&status="+status).success(function(data){
-        getItem();
-      });
-  };*/
+    };
  
 });
 
 app.controller('homeController', function($scope, $http) {
     
-    $scope.customNavigate=function(msg){
+    $scope.customNavigate=function(id){
         
-       $location.path("/vistorias"+msg)
+       $location.path("/vistorias"+id)
     }
 	
   getItem(); // Load all available items 
