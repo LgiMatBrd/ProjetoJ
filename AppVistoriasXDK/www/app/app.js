4cc3a7c84c352f22af2e89dfd5ac039099572ae9
LgiMatBrd <contato@skorp.com.br>
2017-03-09 17:12:47 -0300

    Melhorias no banco de dados local
    Algumas melhorias no sistema de bano de dados local foram implementadas,
    entre elas: limpeza do código, rodar verificação se os bancos existem
    antes e inicializar os controller e criação da 'tabela' vistorias

--- AppVistoriasXDK/www/app/app.js
+++ AppVistoriasXDK/www/app/app.js
@@ -10,12 +10,17 @@ app.config(function($routeProvider) {
     .when("/vistorias/:id?", {
         templateUrl : "paginas/vistorias.html",
 		controller  : 'vistoriasController'
+    })
+    .when("/vistoria/:id?", {
+        templateUrl : "paginas/vistoria.html",
+		controller  : 'vistoriaController'
+    })
+    .otherwise({
+       redirectTo: '/'
     });
 });
 
-app.controller('homeController', function($scope, $http, $localStorage) {
-    
-    /* Luigi, se souber como faz, coloca esse pequeno trecho na parte de app.config */
+app.run(function($localStorage) {
     if (typeof $localStorage.clientes === 'undefined' || typeof $localStorage.clientes.version === 'undefined' || $localStorage.clientes.version !== 'v0.1')
     {
         $localStorage.clientes = {
@@ -24,16 +29,31 @@ app.controller('homeController', function($scope, $http, $localStorage) {
             db: {}
         }; 
     }
-    /* Luigi, o pequeno trecho vai até aqui */
-    
-    $scope.addItem = function ($valor)
+    if (typeof $localStorage.vistorias === 'undefined' || typeof $localStorage.vistorias.version === 'undefined' || $localStorage.vistorias.version !== 'v0.1')
     {
-        
+        $localStorage.vistorias = {
+            nextID: 0,
+            version: 'v0.1',
+            db: {}
+        }; 
+    } 
+});
+
+app.controller('homeController', function($scope, $http, $localStorage, $location) {
+    
+    $scope.verVistorias = function (id) {
+        $location.path('/vistorias/' + id);
+    };
+
+    $scope.addCliente = function($valor)
+    {
+        var data_criacao = new Date();
         id = $localStorage.clientes.nextID;
         
-        cliente = new ClienteObj(); // Definições do objeto estão no arquivo dbObj.js
+        cliente = new Cliente(); // Definições do objeto estão no arquivo dbObj.js
         cliente.id = id;
         cliente.nome = $valor;
+        cliente.data_criacao = data_criacao; 
         $localStorage.clientes.db[id] = cliente;
         
         id = id + 1;
@@ -41,66 +61,75 @@ app.controller('homeController', function($scope, $http, $localStorage) {
         
     };
     
-    $scope.readClientes = function ()
+    $scope.lerClientes = function ()
     {
         return $localStorage.clientes.db;
-    };
+    }; 
     
-    $scope.deleteItem = function ($id)
+    $scope.deletarCliente = function ($id)
     {
+        window.history.back();
         delete $localStorage.clientes.db[$id];
-    };
-    
-  /*$scope.addItem = function (item) {
-    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/clientes/addClientes.php?item="+item).success(function(data){
-        getItem();
-        $scope.itemInput = "";
-      });
-  };
-  
-  $scope.deleteItem = function (item) {
-    if(confirm("Are you sure to delete this item?")){
-    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/clientes/deleteClientes.php?itemID="+item).success(function(data){
-        getItem();
-      });
-    }
-  };
-
-  $scope.clearItem = function () {
-    if(confirm("Delete all checked items?")){
-    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/clientes/clearClientes.php").success(function(data){
-        getItem();
-      });
-    }
-  };  
-
-  $scope.changeStatus = function(item, status, task) {
-    if(status=='2'){status='0';}else{status='2';}
-      $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/clientes/updateClientes.php?itemID="+item+"&status="+status).success(function(data){
-        getItem();
-      });
-  };*/
-
+    };     
+    console.dir($localStorage);
 });
 
-app.controller('vistoriasController', function($scope, $routeParams, $http) {
-	$scope.id= $routeParams.id;
+app.controller('vistoriasController', function($scope, $routeParams, $http, $localStorage) {
+	
+    // id do cliente
+    $scope.id = $routeParams.id;
+    $scope.id_dono = $routeParams.id;
+    
+    // botão de voltar
     $scope.goBack = function() {
         window.history.back();
     };
     
-    getItem(); // Load all available items 
-    function getItem(){  
-    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/vistorias/getVistorias.php?id_cliente="+$scope.id).success(function(data){
-        $scope.items = data;
-       });
-    }
-
-    $scope.addItem = function (item) {
-    $http.post("http://hom.agenciageld.com.br/app_seyconel/ajax/vistorias/addVistorias.php?item="+item).success(function(data){
-        getItem();
-        $scope.itemInput = "";
-      });
+    // adicionar vistoria
+    $scope.addVistoria = function($valor)
+    {
+        var data_criacao = new Date();
+        id = $localStorage.vistorias.nextID;
+        
+        vistoria = new Vistoria(); 
+        vistoria.id = id;
+        vistoria.id_dono = $scope.id_dono; 
+        vistoria.nome = $valor;
+        vistoria.data_criacao = data_criacao; 
+        $localStorage.vistorias.db[id] = vistoria;
+        
+        id = id + 1;
+        $localStorage.vistorias.nextID = id;
+        
+    };
+    
+    // ler vistorias
+    $scope.lerVistorias = function () 
+    {  
+        return $localStorage.vistorias.db;   
+    };
+    
+    // deletar vistoria
+    $scope.deletarVistoria = function ($id)
+    {
+        delete $localStorage.vistorias.db[$id]; 
     };
  
 });
+
+app.directive('ngConfirmClick', [
+    function(){
+        return {
+            link: function (scope, element, attr) {
+                var msg = attr.ngConfirmClick || "Você tem certeza?";
+                var clickAction = attr.confirmedClick;
+                element.bind('click',function (event) {
+                    if ( window.confirm(msg) ) {
+                        scope.$eval(clickAction)
+                    } else {
+                        window.history.back();
+                    }
+                });
+            }
+        };
+}])
--- AppVistoriasXDK/www/app/dbObj.js
+++ AppVistoriasXDK/www/app/dbObj.js
@@ -1,8 +1,18 @@
 // Define e inicializa os objetos a serem usados na DB através de construtores
 
 // Objeto de cliente
-function ClienteObj()
+function Cliente()
 {
     this.id = 0;
     this.nome = '';
-}
+    this.data_criacao = '';
+} 
+
+// Objeto da vistoria
+function Vistoria()
+{
+    this.id = 0;
+    this.nome = '';
+    this.id_dono = '';
+    this.data_criacao = '';
+} 
