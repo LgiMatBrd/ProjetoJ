c15b5840d9423f08313c2f0847ca4fd1e18ca5c6
igorol13 <igor@gamerzbr.com>
2017-04-20 11:29:31 -0300

    Charset alterado para UTF-8 na conexao com a DB

--- WebBackend/config/db_connect.php
+++ WebBackend/config/db_connect.php
@@ -4,3 +4,4 @@ if (!defined('ROOT_DIR'))
 
 include_once ROOT_DIR.'/config/psl-config.php';   // Já que functions.php não está incluso
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+$mysqli->set_charset('utf8');
